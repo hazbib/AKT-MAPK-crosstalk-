7fc64c92a75ee215a2dade1453f262f48283cc2d
hazbib <hamzeh.zbib@pnnl.gov>
2016-12-01 10:21:54 -0800

    R codes

---
+++ R/fmmk.R
@@ -0,0 +1,34 @@
+library(deSolve)
+
+fmodel <- function(lambda, y, parms){
+  dy1 <- -20*y[1]*(y[2]- y[1]*y[2]/(0.05+y[1])) + 0.0909*y[1]*y[2]/(0.05+y[1]) + 0.9091*(1-y[1])/(0.05+(1-y[1]))
+  dy2 <- lambda + (1 - 0.01*0.559 -0.559*0.5*0.81552)*(1-y[1]) - 0.1*0.81552 - y[2]
+  list(c(dy1, dy2))
+}
+
+# d1 = 0.0909
+# k1 = k2 = 0.9091
+# K_1 = K_2 = 0.05
+# beta = 1  
+# E2t = 1 
+# phi = 1 
+# eps = 0.01
+# psi = 0.559
+# kk2 = 338.44
+# kk3 = 420
+# sig = 1
+# erk = 0.81552
+
+# (d1+k1)/K_1 = 20 
+
+#interaction parameters 
+# k2 = 5
+# k3 = 0.1
+
+yj <- c(y1 = 1, y2 = 1)
+lambda <- seq(0,1,0.1)
+time = seq(0,1,0.01)
+
+outf <- ode(times = time, y = yj, func = fmodel, parms = NULL)
+
+plot(outf)
